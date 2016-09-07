f7e4af0efe46c6d7908d81d1f346c9957a232058
Joe Anzalone <joe@shmit.com>
2016-09-07 01:11:34 -0400

    Create new Dog classes

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function preload() {
 var player;
 var cursors;
 var shiftKey;
+var allDogs;
 
 function create() {
     game.world.setBounds(0, 0, 1600, 1200);
@@ -16,63 +17,31 @@ function create() {
 
     game.stage.backgroundColor = '#168a14';
 
-    dogGrey = game.add.sprite(50, 50, 'pets', 0);
-    dogGrey.smoothed = false;
-    game.physics.arcade.enable(dogGrey);
-    dogGrey.body.collideWorldBounds = true;
-    dogGrey.body.setSize(30, 36, 10, 35);
-    dogGrey.animations.add('walk-south', [0, 1, 2, 1], 6, true);
-    dogGrey.animations.add('walk-west', [12, 13, 14, 13], 6, true);
-    dogGrey.animations.add('walk-east', [24, 25, 26, 25], 6, true);
-    dogGrey.animations.add('walk-north', [36, 37, 38, 37], 6, true);
-
-
-    dogYellow = game.add.sprite(100, 50, 'pets', 3);
-    dogYellow.smoothed = false;
-    game.physics.arcade.enable(dogYellow);
-    dogYellow.body.collideWorldBounds = true;
-    dogYellow.body.setSize(30, 36, 10, 35);
-    dogYellow.animations.add('walk-south', [3, 4 , 5, 4], 6, true);
-    dogYellow.animations.add('walk-west', [15, 16, 17, 16], 6, true);
-    dogYellow.animations.add('walk-east', [27, 28, 29, 28], 6, true);
-    dogYellow.animations.add('walk-north', [39, 40, 41, 40], 6, true);
-
-    dogBrown = game.add.sprite(150, 50, 'pets', 6);
-    dogBrown.smoothed = false;
-    game.physics.arcade.enable(dogBrown);
-    dogBrown.body.collideWorldBounds = true;
-    dogBrown.body.setSize(30, 36, 10, 35);
-    dogBrown.animations.add('walk-south', [6, 7, 8, 7], 6, true);
-    dogBrown.animations.add('walk-west', [18, 19, 20 ,19], 6, true);
-    dogBrown.animations.add('walk-east', [30, 31, 32, 31], 6, true);
-    dogBrown.animations.add('walk-north', [42, 43, 44, 43], 6, true);
-
-    dogTwoTone = game.add.sprite(200, 50, 'pets', 9);
-    dogTwoTone.smoothed = false;
-    game.physics.arcade.enable(dogTwoTone);
-    dogTwoTone.body.collideWorldBounds = true;
-    dogTwoTone.body.setSize(30, 36, 10, 35);
-    dogTwoTone.animations.add('walk-south', [9, 10, 11, 10], 6, true);
-    dogTwoTone.animations.add('walk-west', [21, 22, 23, 22], 6, true);
-    dogTwoTone.animations.add('walk-east', [33, 34, 35, 34], 6, true);
-    dogTwoTone.animations.add('walk-north', [45, 46, 47, 46], 6, true);
+    dogGrey = new GreyDog(game, 50, 50);
+    dogYellow = new YellowDog(game, 100, 50);
+    dogBrown = new BrownDog(game, 150, 50);
+    dogTwoTone = new TwoToneDog(game, 200, 50);
 
     player = dogGrey;
+    player.body.immovable = false;
     game.camera.follow(player);
 
     cursors = game.input.keyboard.createCursorKeys();
     shiftKey = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
+
+    allDogs = game.add.group();
+    allDogs.addMultiple([dogGrey, dogYellow, dogBrown, dogTwoTone]);
 }
 
 function update() {
     player.body.velocity.set(0);
 
     if (shiftKey.isDown) {
-        player.animations.currentAnim.speed = 12;
-        player.speed = 200;
+        player.speed = player.baseMovementSpeed * 1.75;
+        player.animations.currentAnim.speed = player.baseAnimationSpeed * 1.75;
     } else {
-        player.speed = 100;
-        player.animations.currentAnim.speed = 6;
+        player.speed = player.baseMovementSpeed;
+        player.animations.currentAnim.speed = player.baseAnimationSpeed;
     }
 
     if (cursors.left.isUp && cursors.right.isUp && cursors.up.isUp && cursors.down.isUp) {
@@ -103,4 +72,67 @@ function update() {
         player.body.velocity.x = player.speed;
     }
 
+    game.physics.arcade.collide(allDogs);
+}
+
+class Dog extends Phaser.Sprite {
+    constructor(game, x, y, frame) {
+        super(game, x, y, 'pets', frame);
+        game.physics.arcade.enable(this);
+        this.body.immovable = true;
+
+        this.smoothed = false;
+        this.body.collideWorldBounds = true;
+        this.body.setSize(30, 36, 10, 35);
+
+        this.animations.add('walk-south', [0, 1, 2, 1], 6, true);
+        this.animations.add('walk-west', [12, 13, 14, 13], 6, true);
+        this.animations.add('walk-east', [24, 25, 26, 25], 6, true);
+        this.animations.add('walk-north', [36, 37, 38, 37], 6, true);
+
+        game.stage.addChild(this);
+
+        this.baseMovementSpeed = 125;
+        this.baseAnimationSpeed = 6;
+    }
+}
+
+class GreyDog extends Dog {
+    constructor(game, x, y) {
+        super(game, x, y, 0);
+        this.animations.add('walk-south', [0, 1, 2, 1], 6, true);
+        this.animations.add('walk-west', [12, 13, 14, 13], 6, true);
+        this.animations.add('walk-east', [24, 25, 26, 25], 6, true);
+        this.animations.add('walk-north', [36, 37, 38, 37], 6, true);
+    }
+}
+
+class YellowDog extends Dog {
+    constructor(game, x, y) {
+        super(game, x, y, 3);
+        this.animations.add('walk-south', [3, 4 , 5, 4], 6, true);
+        this.animations.add('walk-west', [15, 16, 17, 16], 6, true);
+        this.animations.add('walk-east', [27, 28, 29, 28], 6, true);
+        this.animations.add('walk-north', [39, 40, 41, 40], 6, true);
+    }
+}
+
+class BrownDog extends Dog {
+    constructor(game, x, y) {
+        super(game, x, y, 6);
+        this.animations.add('walk-south', [6, 7, 8, 7], 6, true);
+        this.animations.add('walk-west', [18, 19, 20 ,19], 6, true);
+        this.animations.add('walk-east', [30, 31, 32, 31], 6, true);
+        this.animations.add('walk-north', [42, 43, 44, 43], 6, true);
+    }
+}
+
+class TwoToneDog extends Dog {
+    constructor(game, x, y) {
+        super(game, x, y, 9);
+        this.animations.add('walk-south', [9, 10, 11, 10], 6, true);
+        this.animations.add('walk-west', [21, 22, 23, 22], 6, true);
+        this.animations.add('walk-east', [33, 34, 35, 34], 6, true);
+        this.animations.add('walk-north', [45, 46, 47, 46], 6, true);
+    }
 }
